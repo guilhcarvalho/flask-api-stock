bd4c85c2913873775ce666eaa7e8e554692e3a9a
Guilherme <guicarvalhosoares@hotmail.com>
2026-01-06 14:59:55 -0300

    FEAT: Introducing database to project

--- src/app.py
+++ src/app.py
@@ -1,5 +1,23 @@
-from flask import Flask
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import click
 import os
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+@click.command('init-db')
+def init_db_command():
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
 
 
 def create_app(test_config=None):
@@ -7,21 +25,24 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'dji_parts_stock.sqlite')
+        SQLALCHEMY_DATABASE_URI=os.path.join('sqlite:///dji_parts_stock.sqlite')
     )
-    
+
     # Load the instance config, if it exists, when not testing
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-    
+
     # Ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
+    app.cli.add_command(init_db_command)
+    db.init_app(app)
+
     @app.route("/product/<id>/<dji_part_number>/<name>/<compatibility>/<quantity>", methods=["GET"])
     def list_product(id, dji_part_number, name, compatibility, quantity):
         return {
@@ -31,7 +52,7 @@ def create_app(test_config=None):
             'Compatibility': compatibility,
             'Quantity': quantity
         }
-    
+
     @app.route("/create_product/<dji_part_number>/<name>/<compatibility>/<quantity>", methods=['GET', 'POST'])
     def create_product(dji_part_number, name, compatibility, quantity):
         return {
@@ -40,5 +61,5 @@ def create_app(test_config=None):
             'Compatibility': compatibility,
             'Quantity': quantity
         }
-    
+
     return app
