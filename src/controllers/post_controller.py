de8a9ce0685965171372d5406ee51db06a16787e
Guilherme Carvalho <guicarvalhosoares@hotmail.com>
2026-01-07 23:02:18 -0300

    feat: Implementing CRUD commands

--- src/controllers/post_controller.py
+++ src/controllers/post_controller.py
@@ -1,16 +1,40 @@
-from flask import Flask, Blueprint
-from src.models.models import db
+from flask import Flask, Blueprint, request
+from src.models.models import User, Dji_Part, db
+from http import HTTPStatus
 
 app = Blueprint('Post', __name__, url_prefix='/post_controllers')
 
 
-@app.route("/", methods=["POST"])
-def list_product(id, dji_part_number, name, quantity, last_update, author_id):
-    return {
-        'ID': id,
-        'DJI Part Number': dji_part_number,
-        'Name': name,
-        'Quantity': quantity,
-        'Last update': last_update,
-        'Author ID': author_id
-    }
+def _create_user():
+    data = request.json
+    user = User(username=data["username"])
+    with db.session.begin():
+        db.session.add(user)
+        db.session.commit()
+
+
+def _register_item():
+    data = request.json
+    register_item = Dji_Part(
+        dji_part_number=data["dji_part_number"],
+        name=data["name"],
+        quantity=data["quantity"],
+        author_id=data["author_id"]
+    )
+    with db.session.begin():
+        db.session.add(register_item)
+        db.session.commit()
+
+
+@app.route("/create_users", methods=["POST"])
+def handle_user():
+    if request.method == "POST":
+        _create_user()
+        return {"message": "User created!"}, HTTPStatus.CREATED
+
+
+@app.route("/register_item", methods=["POST"])
+def register_item():
+    if request.method == "POST":
+        _register_item()
+        return {"message": "Registered item!"}, HTTPStatus.CREATED
